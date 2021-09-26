e2bc26b837c8f438c96d229fa8d29ed3e7fb4092
Surojit Saha <surojit.aec@gmail.com>
2021-09-26 21:47:30 +0530

    Setup app for Heroku

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 var path = require('path')
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
 app.set('view engine', 'ejs');
 
